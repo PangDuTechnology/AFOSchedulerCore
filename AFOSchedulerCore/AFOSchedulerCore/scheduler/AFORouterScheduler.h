388eb521dcdc159ae1e13705248b66e3336b592c
PangDu <792229197@qq.com>
2019-10-12 22:26:39 +0800

    commit

---
+++ AFOSchedulerCore/AFOSchedulerCore/scheduler/AFORouterScheduler.h
@@ -0,0 +1,17 @@
+//
+//  AFORouterScheduler.h
+//  AFORouter
+//
+//  Created by xianxueguang on 2019/10/11.
+//  Copyright © 2019年 AFO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AFORouterScheduler : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AFOSchedulerCore/AFOSchedulerCore/scheduler/AFORouterScheduler.m
@@ -0,0 +1,13 @@
+//
+//  AFORouterScheduler.m
+//  AFORouter
+//
+//  Created by xianxueguang on 2019/10/11.
+//  Copyright © 2019年 AFO. All rights reserved.
+//
+
+#import "AFORouterScheduler.h"
+#import "NSObject+AFOScheduler.h"
+@implementation AFORouterScheduler
+
+@end
