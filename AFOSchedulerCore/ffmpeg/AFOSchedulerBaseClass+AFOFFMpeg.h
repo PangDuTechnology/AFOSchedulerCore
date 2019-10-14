2cb9bad2c1825b424c2d24ff903b7ea3ce020b00
PangDu <792229197@qq.com>
2019-10-14 13:33:54 +0800

    commit

---
+++ AFOSchedulerCore/ffmpeg/AFOSchedulerBaseClass+AFOFFMpeg.h
@@ -0,0 +1,16 @@
+//
+//  AFOSchedulerBaseClass+AFOFFMpeg.h
+//  AFOSchedulerCore
+//
+//  Created by piccolo on 2019/10/14.
+//  Copyright © 2019 piccolo. All rights reserved.
+//
+#import <AFOSchedulerCore/AFOSchedulerBaseClass.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AFOSchedulerBaseClass (AFOFFMpeg)
+
+@end
+
+NS_ASSUME_NONNULL_END
